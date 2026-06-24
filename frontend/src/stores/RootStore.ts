b72c850cc351da17d4d43050220dc775367d8e9a
Vitaly Dudarev <viduwebjob@gmail.com>
2026-06-25 02:36:41 +0300

    feat(frontend): add vacancy store to RootStore

--- frontend/src/stores/RootStore.ts
+++ frontend/src/stores/RootStore.ts
@@ -1,13 +1,16 @@
 import { AuthStore } from './AuthStore'
 import { CompanyStore } from './CompanyStore'
+import { VacancyStore } from './VacancyStore'
 
 export class RootStore {
   auth: AuthStore
   company: CompanyStore
+  vacancy: VacancyStore
 
   constructor() {
     this.auth = new AuthStore()
     this.company = new CompanyStore()
+    this.vacancy = new VacancyStore()
   }
 }
 
