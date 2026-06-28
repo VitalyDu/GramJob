4863f649bc23a86eebda46b87b8fa35e8c257a6c
Vitaly Dudarev <viduwebjob@gmail.com>
2026-06-29 02:20:45 +0300

    feat(frontend): add ApplicationStore to RootStore

--- frontend/src/stores/RootStore.ts
+++ frontend/src/stores/RootStore.ts
@@ -2,18 +2,21 @@ import { AuthStore } from './AuthStore'
 import { CompanyStore } from './CompanyStore'
 import { VacancyStore } from './VacancyStore'
 import { ResumeStore } from './ResumeStore'
+import { ApplicationStore } from './ApplicationStore'
 
 export class RootStore {
   auth: AuthStore
   company: CompanyStore
   vacancy: VacancyStore
   resume: ResumeStore
+  application: ApplicationStore
 
   constructor() {
     this.auth = new AuthStore()
     this.company = new CompanyStore()
     this.vacancy = new VacancyStore()
     this.resume = new ResumeStore()
+    this.application = new ApplicationStore()
   }
 }
 
